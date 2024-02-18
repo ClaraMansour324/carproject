4fe26f0734235398361830785d10374de14cc542
clara mansour <https://github.com/ClaraMansour324>
2024-02-18 18:23:07 +0200

    pagination = 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users=User::paginate(3);
+        $users=User::paginate(1);
         return view('admin/users',compact('users'));
     }
 
